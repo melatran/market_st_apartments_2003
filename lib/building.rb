a82f234fdd824c4c191ef13c5911f648124be2ec
Melanie Tran <melaniexx02@gmail.com>
2020-03-31 10:55:50 -0600

    Add building methods and tests

--- lib/building.rb
+++ lib/building.rb
@@ -1,14 +1,28 @@
 class Building
-  attr_reader :units
+  attr_reader :units, :rented_units, :renters
+
   def initialize
     @units = []
+    @renters = []
+    @rented_units = []
   end
 
   def add_unit(unit_number)
     @units << unit_number
   end
 
-  def add_renter(renter_name)
-    @units << apartment.renter_name
+  def add_renter(renter)
+    @renters << renter
+  end
+
+  def average_rent (payment)
+    @units.select do |unit|
+      unit.rent == payment
+    end
+    @monthly_rent.to_f/2
+  end
+
+  def rent_units
+    @rented_units << renters
   end
 end
--- test/building_test.rb
+++ test/building_test.rb
@@ -35,7 +35,55 @@ class BuildingTest < Minitest::Test
 
     renter1 = Renter.new("Aurora")
     unit1.add_renter(renter1)
-    assert_equal ["Aurora"], building.renter
+    assert_equal renter1, unit1.renter
 
+    renter2 = Renter.new("Tim")
+    unit2.add_renter(renter2)
+    assert_equal renter2, unit2.renter
+
+    assert_equal ["Aurora", "Tim"], building.renters
+  end
+
+    def test_it_has_average_renter
+      building = Building.new
+      unit1 = Apartment.new({number: "A1", monthly_rent: 1200, bathrooms: 1, bedrooms: 1})
+      unit2 = Apartment.new({number: "B2", monthly_rent: 999, bathrooms: 2, bedrooms: 2})
+      building.add_unit(unit1)
+      building.add_unit(unit2)
+
+      renter1 = Renter.new("Aurora")
+      unit1.add_renter(renter1)
+      renter2 = Renter.new("Tim")
+      unit2.add_renter(renter2)
+
+      assert_equal 1099.5, building.average_rent
+  end
+
+  def test_it_has_rented_units
+    building = Building.new
+    unit1 = Apartment.new({number: "A1", monthly_rent: 1200, bathrooms: 1, bedrooms: 1})
+    unit2 = Apartment.new({number: "B2", monthly_rent: 999, bathrooms: 2, bedrooms: 2})
+    unit3 = Apartment.new({number: "C3", monthly_rent: 1150, bathrooms: 2, bedrooms: 2})
+    unit4 = Apartment.new({number: "D4", monthly_rent: 1500, bathrooms: 2, bedrooms: 3})
+    building.add_unit(unit1)
+    building.add_unit(unit2)
+    building.add_unit(unit3)
+
+    assert_equal [], building.rented_units
+  end
+
+  def test_it_has_renter
+    building = Building.new
+    unit1 = Apartment.new({number: "A1", monthly_rent: 1200, bathrooms: 1, bedrooms: 1})
+    unit2 = Apartment.new({number: "B2", monthly_rent: 999, bathrooms: 2, bedrooms: 2})
+    unit3 = Apartment.new({number: "C3", monthly_rent: 1150, bathrooms: 2, bedrooms: 2})
+    unit4 = Apartment.new({number: "D4", monthly_rent: 1500, bathrooms: 2, bedrooms: 3})
+    building.add_unit(unit1)
+    building.add_unit(unit2)
+    building.add_unit(unit3)
+    renter1 = Renter.new("Spencer")
+    unit2.add_renter(renter1)
+
+    assert_equal [unit2], building.rented_units
   end
 end
