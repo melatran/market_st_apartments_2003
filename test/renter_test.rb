632f3b2f7efce8be33246cf8ceb122ef99e25de6
Melanie Tran <melaniexx02@gmail.com>
2020-03-31 09:14:12 -0600

    Crated apartment class

--- test/renter_test.rb
+++ test/renter_test.rb
@@ -1,6 +1,7 @@
 require 'minitest/autorun'
 require 'minitest/pride'
 require './lib/renter'
+require './lib/apartment'
 
 class RenterTest < Minitest::Test
   def test_it_exists
@@ -11,5 +12,11 @@ class RenterTest < Minitest::Test
   def test_renter_has_name
     renter1 = Renter.new("Jessie")
     assert_equal "Jessie", renter1.name
+  end
+
+  def test_apartment_exists
+    unit1 = Apartment.new({number: "A1", monthly_rent: 1200, bathrooms: 1, bedrooms: 1})
+    
+    assert_instance_of Apartment, unit1
   end
 end
